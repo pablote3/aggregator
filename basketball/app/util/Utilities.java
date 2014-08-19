d6e178a3320d226733697ddec00e25ed48cf6af2
Paul Rossotti <rossotti.paul@gmail.com>
2014-08-19 04:52:16 -0700

    Initial commit

---
+++ basketball/app/util/Utilities.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.math.BigDecimal;
+
+
+public class Utilities {
+	static public boolean isValidNumber(String number)  {
+		try {
+			if (Integer.parseInt(number) >= 0)
+				return true;
+			else
+				return false;
+		} 
+		catch (NumberFormatException e) {
+			return false;
+		}
+	}
+	
+	static public String padString(String text, int length)  {
+		return String.format("%1$-" + length + "s", text);
+	}
+	
+    public static BigDecimal roundtoBigDecimal(float d, int decimalPlace) {
+    	//use to retain number of decimal places with ending zeros
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd;
+    }
+    
+    public static float roundtoFloat(float d, int decimalPlace) {
+    	//use to retain number of decimal places with ending zeros
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+}
