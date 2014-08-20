437d47750dccd059494cb365da4d79f34e4064f7
Paul Rossotti <rossotti.paul@gmail.com>
2014-08-20 05:30:49 -0700

    Added teamList

--- basketball/app/util/Utilities.java
+++ basketball/app/util/Utilities.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 
 
 public class Utilities {
@@ -37,5 +38,40 @@ public class Utilities {
         BigDecimal bd = new BigDecimal(Float.toString(d));
         bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
         return bd.floatValue();
+    }
+    
+    public static ArrayList<String> teamList() {
+    	ArrayList<String> list = new ArrayList<String>();
+    	list.add("BOS");
+    	list.add("BKN");
+    	list.add("NY");
+    	list.add("PHI"); 
+    	list.add("TOR");
+    	list.add("CHI");
+    	list.add("CLE");
+    	list.add("DET");
+    	list.add("IND");
+    	list.add("MIL");
+    	list.add("ATL");
+    	list.add("CHA");
+    	list.add("MIA");
+    	list.add("ORL");
+    	list.add("WAS");
+    	list.add("DAL");
+    	list.add("HOU");
+    	list.add("MEM");
+    	list.add("NO");
+    	list.add("SA");
+    	list.add("DEN");
+    	list.add("MIN");
+    	list.add("POR");
+    	list.add("OKC");
+    	list.add("UTA");
+    	list.add("GS");
+    	list.add("LAC");
+    	list.add("LAL");
+    	list.add("PHO");
+    	list.add("SAC");
+    	return list;
     }
 }
