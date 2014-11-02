ee05897e2aeb3b30f43dcb9227474a85eb6ac205
Paul Rossotti <rossotti.paul@gmail.com>
2014-11-02 12:19:00 -0800

    Introduce ebean service injection

---
+++ basketball/app/services/EbeanServerServiceImpl.java
@@ -0,0 +1,31 @@
+package services;
+
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.EbeanServerFactory;
+import com.avaje.ebean.config.DataSourceConfig;
+import com.avaje.ebean.config.ServerConfig;
+
+public class EbeanServerServiceImpl implements EbeanServerService {
+	EbeanServer server;
+	
+	public EbeanServerServiceImpl() {
+	}
+
+	public EbeanServer createEbeanServer() {
+		ServerConfig config = new ServerConfig();
+		config.setName("aggregate"); 
+		
+		DataSourceConfig mySql = new DataSourceConfig();  
+		mySql.setDriver("com.mysql.jdbc.Driver");
+		mySql.setUsername("root");  
+		mySql.setPassword("root"); 
+		mySql.setUrl("jdbc:mysql://localhost:3306/aggregate?characterEncoding=UTF-8");  
+		mySql.setHeartbeatSql("select count(*) from team_box_score");  		  
+		config.setDataSourceConfig(mySql);
+		config.setDefaultServer(false);  
+		config.setRegister(false);
+		
+		EbeanServer server = EbeanServerFactory.create(config);
+		return server;
+	}
+}
---
+++ basketball/app/services/InjectorModule.java
@@ -0,0 +1,11 @@
+package services;
+
+import com.google.inject.AbstractModule;
+
+
+public class InjectorModule extends AbstractModule {
+	@Override 
+	protected void configure() {
+		bind(EbeanServerService.class).to(EbeanServerServiceImpl.class);
+	}
+}
